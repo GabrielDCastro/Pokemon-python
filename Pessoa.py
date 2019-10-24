64812f7edeab543b0667bca03896b6b312237f6d
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-10-23 21:35:50 -0300

    Random  enemys and pokemons

--- Pessoa.py
+++ Pessoa.py
@@ -5,6 +5,18 @@ NOMES=["Dahaka", "Silverdick", "Trandor", "Alain", "Habbit", "Mirra", "Crystal",
            "Sharon", "Mia", "pifsa", "Anônimo", "Azog", "Meklan", "Phara"
     ]
 
+POKEMONS =[
+    PokemonFogo("charmander"),
+    PokemonFogo("flarion"),
+    PokemonFogo("charmilion"),
+    PokemonFogo("charizard"),
+    PokemonEletrico("pikachu"),
+    PokemonEletrico("raichu"),
+    PokemonAgua("squirtle"),
+    PokemonAgua("magicarp"),
+    PokemonAgua("bubasauro"),
+]
+
 class Pessoa:
 
     def __init__(self, nome=None, pokemons=[]):#como podemos ter vários pokemons, então tem que ser uma lista
@@ -35,6 +47,13 @@ class Player(Pessoa):
 class Inimigo(Pessoa):
     tipo="inimigo"
 
+    def __init__(self, nome=None, pokemons=[]):
+        if not pokemons:
+            for i in range(random.randint(1,3)):
+                pokemons.append(random.choice(POKEMONS))
+
+        super().__init__(nome=nome, pokemons=pokemons)
+
 meu_pokemon=PokemonEletrico("pakachu")
 meu_pokemon2=PokemonFogo("charmander")
 eu = Player(nome="Gabriel", pokemons=[meu_pokemon, meu_pokemon2])
@@ -42,3 +61,7 @@ pokemon_selvagem=PokemonAgua("bubasauro")
 eu.mostrar_pokemons()
 eu.capturar(pokemon_selvagem)
 eu.mostrar_pokemons()
+
+meu_inimigo=Inimigo()
+print(meu_inimigo)
+meu_inimigo.mostrar_pokemons()
