a6ab89ed41563d75090c9c98c9fe4d5be7bf99db
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-10-25 20:59:39 -0300

    update battle

--- Pokemon.py
+++ Pokemon.py
@@ -12,23 +12,38 @@ class Pokemon:
             self.nome = nome
         else:
             self.nome = especie
+
+        self.ataque = (20 + (self.level + 5))
+        self.vida = (100 + (self.level + 15))
+
+
     def __str__(self):
         return "{} ({})".format(self.nome, self.level)
 
     def atacar(self, pokemon):
-        print("{} atacou {}".format(self, pokemon))
+        pokemon.vida = pokemon.vida - self.ataque
+        print("{} perdeu {} pontos de vida!\n".format(pokemon, self.ataque))
+
+        if pokemon.vida <= 0:
+            print("{} foi derrotado".format(pokemon))
+            return True
+        else:
+            return False
 
 class PokemonEletrico(Pokemon):
     tipo = "elétrico"
     def atacar(self, pokemon):
-        print("{} lançou um raio do trovão em {}".format(self, pokemon))
+        print("{} lançou um raio do trovão em {}\n".format(self, pokemon))
+        return super().atacar(pokemon)
 
 class PokemonFogo(Pokemon):
     tipo = "fogo"
     def atacar(self, pokemon):
-        print("{} lançou uma bola de fogo em {}".format(self, pokemon))
+        print("{} lançou uma bola de fogo em {}\n".format(self, pokemon))
+        return super().atacar(pokemon)
 
 class PokemonAgua(Pokemon):
     tipo = "água"
     def atacar(self, pokemon):
-        print("{} lançou um jato d'água em {}".format(self, pokemon))
+        print("{} lançou um jato d'água em {}\n".format(self, pokemon))
+        return super().atacar(pokemon)
